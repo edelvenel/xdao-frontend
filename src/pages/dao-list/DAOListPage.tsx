abd05a6c95126c656ba86bc3803e1d86f7182b88
Tatiana K <edelvenel@yandex.ru>
2025-04-29 14:21:53 +0700

    proposal-list and dao-list empty placeholder

--- src/pages/dao-list/DAOListPage.tsx
+++ src/pages/dao-list/DAOListPage.tsx
@@ -1,79 +1,84 @@
-import { TopContent } from "app/navigation/components/top-content";
-import { routes } from "app/router/routes";
-import { Filter } from "pages/proposal-list/components/Filter";
-import { SearchBlock } from "pages/proposal-list/components/SearchBlock";
-import React from "react";
-import { generatePath, useNavigate } from "react-router";
-import { useDaos } from "shared/api/daos/useDaos";
-import { store } from "shared/store";
-import { Modal } from "shared/ui/Modal";
-import { DAO } from "./components/DAO";
-import css from "./styles.module.scss";
+import { TopContent } from 'app/navigation/components/top-content';
+import { routes } from 'app/router/routes';
+import { Filter } from 'pages/proposal-list/components/Filter';
+import { SearchBlock } from 'pages/proposal-list/components/SearchBlock';
+import React from 'react';
+import { generatePath, useNavigate } from 'react-router';
+import { useDaos } from 'shared/api/daos/useDaos';
+import { store } from 'shared/store';
+import { Modal } from 'shared/ui/Modal';
+import { DAO } from './components/DAO';
+import css from './styles.module.scss';
 
 const FILTER_OPTIONS: string[] = [
-  "All DAOs",
-  "My DAOs",
-  "With my LP tokens",
-  "With my GP role",
-  "Rejected",
-  "With active proposals",
+	'All DAOs',
+	'My DAOs',
+	'With my LP tokens',
+	'With my GP role',
+	'Rejected',
+	'With active proposals',
 ];
 
 export const DAOListPage = React.memo(function DAOListPage() {
-  const [searchText, setSearchText] = React.useState<string>("");
-  const [isFilterOpen, setIsFilterOpen] = React.useState<boolean>(false);
-  const [filter, setFilter] = React.useState<number>(0);
-  const { setIsHeaderShown, setIsMenuShown } = store.useApp();
-  const { daos, fetchDaos } = useDaos();
+	const [searchText, setSearchText] = React.useState<string>('');
+	const [isFilterOpen, setIsFilterOpen] = React.useState<boolean>(false);
+	const [filter, setFilter] = React.useState<number>(0);
+	const { setIsHeaderShown, setIsMenuShown, setIsBackground } = store.useApp();
+	const { daos, fetchDaos } = useDaos();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleOnCreate = React.useCallback(() => {
-    navigate(routes.createDao);
-  }, [navigate]);
+	const handleOnCreate = React.useCallback(() => {
+		navigate(routes.createDao);
+	}, [navigate]);
 
-  React.useEffect(() => {
-    setIsMenuShown(true);
-    setIsHeaderShown(true);
-  }, [setIsHeaderShown, setIsMenuShown]);
+	React.useEffect(() => {
+		setIsMenuShown(true);
+		setIsHeaderShown(true);
+	}, [setIsHeaderShown, setIsMenuShown]);
 
-  React.useEffect(() => {
-    fetchDaos();
-  }, [fetchDaos]);
+	React.useEffect(() => {
+		if (daos.length > 0) {
+			setIsBackground(false);
+		} else {
+			setIsBackground(true);
+		}
+	}, [daos.length, setIsBackground]);
 
-  return (
-    <div className={css.page}>
-      <div className={css.list}>
-        {daos.map((dao) => (
-          <DAO
-            key={dao.id}
-            dao={dao}
-            onOpen={() =>
-              navigate(
-                generatePath(routes.dao, { id: dao.id, tab: "overview" })
-              )
-            }
-          />
-        ))}
-      </div>
-      <TopContent>
-        <SearchBlock
-          searchText={searchText}
-          onChange={setSearchText}
-          onFilter={() => setIsFilterOpen(true)}
-          onCreate={handleOnCreate}
-        />
-      </TopContent>
-      {isFilterOpen && (
-        <Modal title="Filter" onClose={() => setIsFilterOpen(false)}>
-          <Filter
-            selected={filter}
-            options={FILTER_OPTIONS}
-            onApply={setFilter}
-            onClose={() => setIsFilterOpen(false)}
-          />
-        </Modal>
-      )}
-    </div>
-  );
+	React.useEffect(() => {
+		fetchDaos();
+	}, [fetchDaos]);
+
+	return (
+		<div className={css.page}>
+			<div className={css.list}>
+				{daos.map((dao) => (
+					<DAO
+						key={dao.id}
+						dao={dao}
+						onOpen={() => navigate(generatePath(routes.dao, { id: dao.id, tab: 'overview' }))}
+					/>
+				))}
+				{daos.length === 0 && <div className={css.placeholder}>No DAOs</div>}
+			</div>
+			<TopContent>
+				<SearchBlock
+					searchText={searchText}
+					onChange={setSearchText}
+					onFilter={() => setIsFilterOpen(true)}
+					onCreate={handleOnCreate}
+				/>
+			</TopContent>
+			{isFilterOpen && (
+				<Modal title="Filter" onClose={() => setIsFilterOpen(false)}>
+					<Filter
+						selected={filter}
+						options={FILTER_OPTIONS}
+						onApply={setFilter}
+						onClose={() => setIsFilterOpen(false)}
+					/>
+				</Modal>
+			)}
+		</div>
+	);
 });
--- src/pages/proposal-list/ProposalList.tsx
+++ src/pages/proposal-list/ProposalList.tsx
@@ -1,74 +1,74 @@
-import { TopContent } from "app/navigation/components/top-content";
-import { routes } from "app/router/routes";
-import React from "react";
-import { useNavigate } from "react-router";
-import { useProposals } from "shared/api/proposals";
-import { store } from "shared/store";
-import { Modal } from "shared/ui/Modal";
-import { Filter } from "./components/Filter";
-import { Proposal } from "./components/Proposal";
-import { SearchBlock } from "./components/SearchBlock";
-import css from "./styles.module.scss";
+import { TopContent } from 'app/navigation/components/top-content';
+import { routes } from 'app/router/routes';
+import React from 'react';
+import { useNavigate } from 'react-router';
+import { useProposals } from 'shared/api/proposals';
+import { store } from 'shared/store';
+import { Modal } from 'shared/ui/Modal';
+import { Filter } from './components/Filter';
+import { Proposal } from './components/Proposal';
+import { SearchBlock } from './components/SearchBlock';
+import css from './styles.module.scss';
 
-const FILTER_OPTIONS: string[] = [
-  "All proposals",
-  "Active",
-  "Pending",
-  "Executed",
-  "Rejected",
-  "My Proposals",
-];
+const FILTER_OPTIONS: string[] = ['All proposals', 'Active', 'Pending', 'Executed', 'Rejected', 'My Proposals'];
 
 export const ProposalListPage = React.memo(function ProposalListPage() {
-  const [searchText, setSearchText] = React.useState<string>("");
-  const [isFilterOpen, setIsFilterOpen] = React.useState<boolean>(false);
-  const [filter, setFilter] = React.useState<number>(0);
-  const { proposals, fetchProposals } = useProposals();
+	const [searchText, setSearchText] = React.useState<string>('');
+	const [isFilterOpen, setIsFilterOpen] = React.useState<boolean>(false);
+	const [filter, setFilter] = React.useState<number>(0);
+	const { proposals, fetchProposals } = useProposals();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const { setIsMenuShown, setIsHeaderShown, setIsBackground } = store.useApp();
-  //TODO: if proposal list is empty change isBackground = true, else - isBackground = false
+	const { setIsMenuShown, setIsHeaderShown, setIsBackground } = store.useApp();
 
-  const handleOnCreate = React.useCallback(() => {
-    navigate(routes.createProposal);
-  }, [navigate]);
+	const handleOnCreate = React.useCallback(() => {
+		navigate(routes.createProposal);
+	}, [navigate]);
 
-  React.useEffect(() => {
-    setIsBackground(false);
-    setIsHeaderShown(true);
-    setIsMenuShown(true);
-  }, [setIsBackground, setIsHeaderShown, setIsMenuShown]);
+	React.useEffect(() => {
+		setIsHeaderShown(true);
+		setIsMenuShown(true);
+	}, [setIsHeaderShown, setIsMenuShown]);
 
-  React.useEffect(() => {
-    fetchProposals();
-  }, [fetchProposals]);
+	React.useEffect(() => {
+		fetchProposals();
+	}, [fetchProposals]);
 
-  return (
-    <div className={css.page}>
-      <div className={css.list}>
-        {proposals.map((proposal, index) => (
-          <Proposal data={proposal} key={index} />
-        ))}
-      </div>
-      <TopContent>
-        <SearchBlock
-          searchText={searchText}
-          onChange={setSearchText}
-          onFilter={() => setIsFilterOpen(true)}
-          onCreate={handleOnCreate}
-        />
-      </TopContent>
-      {isFilterOpen && (
-        <Modal title="Filter" onClose={() => setIsFilterOpen(false)}>
-          <Filter
-            selected={filter}
-            options={FILTER_OPTIONS}
-            onApply={setFilter}
-            onClose={() => setIsFilterOpen(false)}
-          />
-        </Modal>
-      )}
-    </div>
-  );
+	React.useEffect(() => {
+		if (proposals.length > 0) {
+			setIsBackground(false);
+		} else {
+			setIsBackground(true);
+		}
+	}, [proposals.length, setIsBackground]);
+
+	return (
+		<div className={css.page}>
+			<div className={css.list}>
+				{proposals.map((proposal, index) => (
+					<Proposal data={proposal} key={index} />
+				))}
+				{proposals.length === 0 && <div className={css.placeholder}>No active votes</div>}
+			</div>
+			<TopContent>
+				<SearchBlock
+					searchText={searchText}
+					onChange={setSearchText}
+					onFilter={() => setIsFilterOpen(true)}
+					onCreate={handleOnCreate}
+				/>
+			</TopContent>
+			{isFilterOpen && (
+				<Modal title="Filter" onClose={() => setIsFilterOpen(false)}>
+					<Filter
+						selected={filter}
+						options={FILTER_OPTIONS}
+						onApply={setFilter}
+						onClose={() => setIsFilterOpen(false)}
+					/>
+				</Modal>
+			)}
+		</div>
+	);
 });
