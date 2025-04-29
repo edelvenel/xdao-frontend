6a549b2c0f051a9d57b65641c7d7a2a8c9ea34f7
Tatiana K <edelvenel@yandex.ru>
2025-04-29 14:49:46 +0700

    splash loader added

--- src/app/splash/SplashScreen.tsx
+++ src/app/splash/SplashScreen.tsx
@@ -1,31 +1,41 @@
-import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
-import React from "react";
-import { store } from "shared/store";
-import css from "./styles.module.scss";
+import { useTonConnectUI, useTonWallet } from '@tonconnect/ui-react';
+import { ScreenLoader } from 'pages/tech/sceen-loader';
+import React from 'react';
+import { store } from 'shared/store';
+import css from './styles.module.scss';
 
 export function SplashScreen() {
-  const { isWalletConnected, connectWallet } = store.useWallet();
-  const [tonConnectUI] = useTonConnectUI();
-  const wallet = useTonWallet();
+	const { isWalletConnected, isWalletChecked, setIsWalletChecked, connectWallet } = store.useWallet();
+	const [tonConnectUI] = useTonConnectUI();
+	const wallet = useTonWallet();
 
-  const handleOnWalletConnect = React.useCallback(async () => {
-    await tonConnectUI.openSingleWalletModal("telegram-wallet");
-  }, [tonConnectUI]);
+	const handleOnWalletConnect = React.useCallback(async () => {
+		await tonConnectUI.openSingleWalletModal('telegram-wallet');
+	}, [tonConnectUI]);
 
-  if (wallet) {
-    if (!isWalletConnected) {
-      connectWallet(wallet);
-    }
-    return null;
-  }
+	React.useEffect(() => {
+		if (wallet) {
+			if (!isWalletConnected) {
+				connectWallet(wallet);
+			}
+			setIsWalletChecked();
+		} else {
+			setInterval(setIsWalletChecked, 1000);
+		}
+	}, [connectWallet, isWalletConnected, setIsWalletChecked, wallet]);
 
-  return (
-    <div className={css.splash}>
-      <div className={css.figures}>
-        <div className={css.button} onClick={handleOnWalletConnect}>
-          <span>Connect Wallet</span>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			{isWalletChecked && (
+				<div className={css.splash}>
+					<div className={css.figures}>
+						<div className={css.button} onClick={handleOnWalletConnect}>
+							<span>Connect Wallet</span>
+						</div>
+					</div>
+				</div>
+			)}
+			{!isWalletChecked && <ScreenLoader />}
+		</>
+	);
 }
