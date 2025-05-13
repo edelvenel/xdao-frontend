9874cfd03a2cd25a84e82086c290b92cc2073569
Tatiana K <edelvenel@yandex.ru>
2025-05-13 18:32:19 +0700

    raw data remove, reused code fix

--- src/pages/proposal/components/ProposalDetails/components/TransferGPDetail/TransferGPDetail.tsx
+++ src/pages/proposal/components/ProposalDetails/components/TransferGPDetail/TransferGPDetail.tsx
@@ -1,53 +1,23 @@
 import { format } from 'date-fns';
-import { ScreenLoader } from 'pages/tech/sceen-loader';
-import React from 'react';
 import { useNavigate } from 'react-router';
-import { getDao } from 'shared/api/daos/methods';
-import { getDaoProposalVotes } from 'shared/api/proposals/methods';
 import { proposalNameMapper } from 'shared/constants';
 import { ProposalDetailLayout } from 'shared/layouts/proposal-detail-layout';
-import { store } from 'shared/store';
 import { IDao, IProposal, IVote } from 'shared/types';
-import { Collapse } from 'shared/ui/Collapse';
 import { Copy } from 'shared/ui/Copy';
 import css from '../../styles.module.scss';
 import { FormHeader } from '../FormHeader';
 import { SignaturesBlock } from '../SignaturesBlock';
 
 interface ITransferGPDetailProps {
+	dao: IDao;
+	votes: IVote[];
 	proposal: IProposal;
 	onVote: () => void;
 }
 
-export function TransferGPDetail({ proposal, onVote }: ITransferGPDetailProps) {
-	const [isOpen, setIsOpen] = React.useState<boolean>(false);
-	const [votes, setVotes] = React.useState<IVote[] | null>(null);
-	const [dao, setDao] = React.useState<IDao | null> (null);
-	const {token} = store.useAuth();
-
+export function TransferGPDetail({ dao, votes, proposal, onVote }: ITransferGPDetailProps) {
 	const navigate = useNavigate();
 	const formatedCreatedAt = format(new Date(proposal.createdAt), 'LLL dd, yyyy | HH:mm');
-
-		React.useEffect(()=>{
-			const fetchVotes = async() => {
-				if (token !== null) {
-					const votes = await getDaoProposalVotes(token, proposal.daoAddress, proposal.id);
-					setVotes(votes);
-				}
-			}
-			const fetchDao = async() => {
-				if (token !== null) {
-					const dao = await getDao(token, proposal.daoAddress);
-					setDao(dao);
-				}
-			}
-			 fetchVotes();
-			 fetchDao();
-		},[proposal.daoAddress, proposal.id, token])
-	
-		if (votes === null || dao === null) {
-			return <ScreenLoader/>
-		}
 
 	return (
 		<ProposalDetailLayout
@@ -61,7 +31,7 @@ export function TransferGPDetail({ proposal, onVote }: ITransferGPDetailProps) {
 					name={proposal.name}
 					description={proposal.description}
 					status={proposal.status}
-					consensus={proposal.consensus / Number(dao?.LPTokens) * 100}
+					consensus={(proposal.consensus / Number(dao?.LPTokens)) * 100}
 					endDate={proposal.endDate}
 				/>
 
@@ -113,19 +83,6 @@ export function TransferGPDetail({ proposal, onVote }: ITransferGPDetailProps) {
 							<div className={css.value}>{formatedCreatedAt}</div>
 						</div>
 					</div>
-					<Collapse label="Transaction details" isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}>
-						<div className={css.details}>
-							<div className={css.title}>Function: mint (address, uint256)</div>
-							<div className={css.divider} />
-							<div className={css.subtitle}>
-								<span>Raw data</span>
-								<Copy text="0x57584386yfeg0000000000000000000x57584386yfeg0000000000000000000x57584386yfeg0000000000000000000x57584386yfeg00000000000000000034567890453" />
-							</div>
-							<div className={css.rawData}>
-								0x57584386yfeg0000000000000000000x57584386yfeg0000000000000000000x57584386yfeg0000000000000000000x57584386yfeg00000000000000000034567890453
-							</div>
-						</div>
-					</Collapse>
 				</div>
 
 				<SignaturesBlock dao={dao} votes={votes} />
