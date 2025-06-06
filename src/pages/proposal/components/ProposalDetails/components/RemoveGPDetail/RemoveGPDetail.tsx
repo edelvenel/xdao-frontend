d30fcb3b8e9f00bfdf08fde670a94a48e7396e4f
Tatiana K <edelvenel@yandex.ru>
2025-06-06 19:10:52 +0700

    owner address hotfix

--- src/pages/proposal/components/ProposalDetails/components/RemoveGPDetail/RemoveGPDetail.tsx
+++ src/pages/proposal/components/ProposalDetails/components/RemoveGPDetail/RemoveGPDetail.tsx
@@ -1,7 +1,9 @@
 import { format } from 'date-fns';
+import React from 'react';
 import { useNavigate } from 'react-router';
 import { proposalNameMapper } from 'shared/constants';
 import { ProposalDetailLayout } from 'shared/layouts/proposal-detail-layout';
+import { store } from 'shared/store';
 import { IProposal, IVote } from 'shared/types';
 import { Copy } from 'shared/ui/Copy';
 import { formatNumber, getUserFriendlyAddress, shortenAddress } from 'shared/utils/formatters';
@@ -17,8 +19,23 @@ interface IRemoveGPDetailProps {
 }
 
 export function RemoveGPDetail({ votes, proposal, userVote, onVote }: IRemoveGPDetailProps) {
+	const { holders } = store.useFormType();
 	const navigate = useNavigate();
 	const formatedCreatedAt = format(new Date(proposal.createdAt), 'LLL dd, yyyy | HH:mm');
+
+	const getOwnerAddressByJettonAddres = React.useCallback(
+		(jettonAddress: string) => {
+			const holder = holders?.find(
+				(holder) => getUserFriendlyAddress(holder.jetton_wallet_address) === getUserFriendlyAddress(jettonAddress)
+			);
+			if (holder) {
+				return getUserFriendlyAddress(holder.owner_address);
+			}
+			return '';
+		},
+		[holders]
+	);
+
 	return (
 		<ProposalDetailLayout isVotingEnabled={true} userVote={userVote} onBack={() => navigate(-1)} onVote={onVote}>
 			<div className={css.page}>
@@ -41,12 +58,12 @@ export function RemoveGPDetail({ votes, proposal, userVote, onVote }: IRemoveGPD
 						<div className={css.column}>
 							<div className={css.label}>GP Address</div>
 							<div className={css.value}>
-								{proposal.data?.address ? shortenAddress(getUserFriendlyAddress(proposal.data?.address)) : 'NO DATA'}
+								{proposal.data?.address
+									? shortenAddress(getOwnerAddressByJettonAddres(proposal.data?.address) ?? '')
+									: 'NO DATA'}
 							</div>
 						</div>
-						<Copy
-							text={proposal.data?.address ? shortenAddress(getUserFriendlyAddress(proposal.data?.address)) : 'NO DATA'}
-						/>
+						<Copy text={proposal.data?.address ? getOwnerAddressByJettonAddres(proposal.data?.address) : 'NO DATA'} />
 					</div>
 					<div className={css.block}>
 						<div className={css.column}>
